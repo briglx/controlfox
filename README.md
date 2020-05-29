b1886735df96cd0bc522ac9583a66d7619a5c750
Brig Lamoreaux <bklamoreaux@gmail.com>
2020-05-29 14:48:41 -0700

    Add service principal credentials

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Sample to show how to use a docker image to run a powershell command to check RBAC roles on Azure
 
+# Development Setup
+
+Make a copy of local.env.example and rename to local.env. Edit the file with the necessary credentials for the service principal.
+
 # Using Docker 
 
 Build the image
@@ -19,7 +23,7 @@ docker run --rm -it  controlfox:latest
 docker run --rm -it --env-file local.env controlfox:latest
 ```
 
-
 # References
 
 - Powershell Authentication https://docs.microsoft.com/en-us/powershell/azure/authenticate-azureps?view=azps-4.1.0
+- Authenicate with Json https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-authenticate?tabs=bash#authenticate-with-a-json-file
--- Scripts/EH.NameSpace.ps1
+++ Scripts/EH.NameSpace.ps1
@@ -1,10 +1,32 @@
-#Connect-AzAccount
+function Confirm-Credentials {
 
-$date = (Get-Date -format "yyyyMMddHHmm")
-$outfilepath = "/Users/H274270/Documents/OneDrive - Honeywell/HON-Documents/DPS/Scripts/Output/Az.EventHub.NameSpace.Settings-" + $date + ".csv"
-$report = @()
+   if ([string]::IsNullOrWhiteSpace($Env:AZURE_AUTH)){
+      Throw "Missing Azure Auth. Ensure Environment Variable AZURE_AUTH is set"        
+   }
+   
+   $azure_auth = $Env:AZURE_AUTH | ConvertFrom-Json
 
+   if ([string]::IsNullOrWhiteSpace($azure_auth.clientId)){
+      Throw "Missing Azure Auth ClientID. Ensure Environment Variable AZURE_AUTH is set"        
+   }
+   if ([string]::IsNullOrWhiteSpace($azure_auth.clientSecret)){
+      Throw "Missing Azure Auth Client Secret. Ensure Environment Variable AZURE_AUTH is set"        
+   }
+   if ([string]::IsNullOrWhiteSpace($azure_auth.tenantId)){
+      Throw "Missing Azure Auth Tenant. Ensure Environment Variable AZURE_AUTH is set"        
+   }
+}
 
+# Validate Azure Credentials
+Confirm-Credentials
+
+# Authenticate Service Principal
+$azure_auth = $Env:AZURE_AUTH | ConvertFrom-Json
+$secureClientSecret = ConvertTo-SecureString $azure_auth.clientSecret -AsPlainText -Force
+$pscredential = New-Object System.Management.Automation.PSCredential($azure_auth.clientId, $secureClientSecret)
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $azure_auth.tenantId
+
+# Get all subscriptions
 $subscriptions = Get-AzSubscription
 #Use this script to limit to a specific tenant ID (Example is for the HON master tenant)
 #$subscriptions = Get-AzSubscription -TenantId 96ece526-9c7d-48b0-8daf-8b93c90a5d18
@@ -12,6 +34,8 @@ $subscriptions = Get-AzSubscription
 #Use this script to collect from a list of subscriptions. Always use "SubscriptionId" cloumn name and Sub ID values
 #$infilepath = "/Users/" + $sdate + ".csv"
 #$subscriptions = (Get-Content -Path $infilepath)
+
+$report = @()
 
 ForEach ($vsub in $subscriptions)
 {
@@ -31,5 +55,6 @@ ForEach ($vsub in $subscriptions)
 }
 
 #write-host $report
-
+$date = (Get-Date -format "yyyyMMddHHmm")
+$outfilepath = "Az.EventHub.NameSpace.Settings-" + $date + ".csv"
 $report |Export-Csv -Path $outfilepath -Delimiter "," -Force -NoTypeInformation
